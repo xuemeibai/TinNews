d023d9b5bb9efc2cde6e283297ab03f32a8545f1
xuemeibai <59850121+xuemeibai@users.noreply.github.com>
2020-07-18 16:05:13 -0400

    Add three fragments in main activity

--- app/src/main/java/com/example/tinnews/model/NewsResponse.java
+++ app/src/main/java/com/example/tinnews/model/NewsResponse.java
@@ -1,6 +1,7 @@
 package com.example.tinnews.model;
 
 import java.util.List;
+import java.util.Objects;
 
 
 public class NewsResponse {
@@ -10,5 +11,31 @@ public class NewsResponse {
     public String code;
     public String message;
     public String status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NewsResponse)) return false;
+        NewsResponse that = (NewsResponse) o;
+        return Objects.equals(articles, that.articles) &&
+                Objects.equals(code, that.code) &&
+                Objects.equals(message, that.message) &&
+                Objects.equals(status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(articles, code, message, status);
+    }
+
+    @Override
+    public String toString() {
+        return "NewsResponse{" +
+                "articles=" + articles +
+                ", code='" + code + '\'' +
+                ", message='" + message + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
 
