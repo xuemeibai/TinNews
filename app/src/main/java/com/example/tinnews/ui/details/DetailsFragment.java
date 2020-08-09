8f5ed9f3d092bb6c139df5bf9bc7560738c876ef
xuemeibai <59850121+xuemeibai@users.noreply.github.com>
2020-08-08 22:34:13 -0400

    Update the styles of the app

--- app/src/main/java/com/example/tinnews/ui/details/DetailsFragment.java
+++ app/src/main/java/com/example/tinnews/ui/details/DetailsFragment.java
@@ -1,24 +1,21 @@
 package com.example.tinnews.ui.details;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.tinnews.R;
 import com.example.tinnews.databinding.FragmentDetailsBinding;
 import com.example.tinnews.model.Article;
 import com.squareup.picasso.Picasso;
 
-
 public class DetailsFragment extends Fragment {
     private FragmentDetailsBinding binding;
-
     public DetailsFragment() {
         // Required empty public constructor
     }
@@ -28,6 +25,14 @@ public class DetailsFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -42,5 +47,3 @@ public class DetailsFragment extends Fragment {
     }
 
 }
-
-
